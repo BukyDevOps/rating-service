2946fac484be63d821e898b96ddf205f17094e0d
Luka Kureljušić <lkureljusic@ymail.com>
2023-07-24 19:30:19 +0200

    initial commit

---
+++ src/main/java/buky/example/ratingservice/RatingServiceApplication.java
@@ -0,0 +1,13 @@
+package buky.example.ratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+	}
+
+}
