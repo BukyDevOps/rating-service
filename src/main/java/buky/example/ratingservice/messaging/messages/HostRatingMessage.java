1120ad5622c0b5e47a7be44ca6f197e578eb4648
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2023-08-01 14:48:59 +0200

    feat: rating notifications implemented

---
+++ src/main/java/buky/example/ratingservice/messaging/messages/HostRatingMessage.java
@@ -0,0 +1,19 @@
+package buky.example.ratingservice.messaging.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HostRatingMessage implements Serializable {
+
+    private Long userId;
+    private Long hostId;
+    private Long ratingId;
+    private Byte ratingValue;
+    private Byte oldRatingValue;
+}
